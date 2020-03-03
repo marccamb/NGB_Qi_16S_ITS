ec2986e4fbb6c49e45da3845f53b481d7372b30f
marcamb <cambonmarine@gmail.com>
2020-03-03 10:46:35 +0100

    Corrected a bug in the function that lead to false mean and sd of error rate

--- src_function_rf_mtry_taxo.R
+++ src_function_rf_mtry_taxo.R
@@ -9,8 +9,9 @@
 rf.mtry.taxo <- function(tab, tax.table, treat,
                          n_fold = 5, 
                          n_mtry = 5,
+                         train.id = NA, 
                          seed=1409) {
-  # For debugging:
+  # # For debugging:
   # l <- "ASV"
   # i <- 1
   # n <- 1
@@ -20,12 +21,16 @@ rf.mtry.taxo <- function(tab, tax.table, treat,
   # tax.table <- taxo
   # variable <- "irrigation"
   # treat <- z$irrigation
+  # train.id <- "-M-"
   
+  if (is.na(train.id)) {
+    set.seed(seed)
+    train.idx <- sample(rep(1:n_fold, 1/n_fold * ncol(tab)), replace = F)
+  } else {
+    train.idx <- grep(train.id, colnames(tab))
+  }
   
-  set.seed(seed)
-  train.idx <- sample(rep(1:n_fold, 1/n_fold * ncol(tab)), replace = F)
   res_tot <- list()
-  
   for (l in c("ASV", "genus", "family", "order", "class")) {
     if(l=="ASV") {
       tab_agg <- tab
@@ -35,16 +40,32 @@ rf.mtry.taxo <- function(tab, tax.table, treat,
     tab_agg <- data.frame("treat" = treat, t(tab_agg))
     if (n_mtry+1>ncol(tab_agg)) n_mtry <- ncol(tab_agg)-1 
     mtry <- 1:n_mtry*(ncol(tab_agg)-1)/n_mtry
-    error <- data.frame()
-    TP <- TN <- FP <- FN <- NULL
-    rate <- NULL
     err_mean <- NULL
     err_sd <- NULL
+    TP <- TN <- FP <- FN <- NULL
     
     for (n in 1:n_mtry) {
-      for (i in 1:n_fold) {
-        train <- tab_agg[train.idx != i, ]
-        test <- tab_agg[train.idx == i, ]
+      error <- data.frame()
+      rate <- NULL
+      if (is.na(train.id)) {
+        for (i in 1:n_fold) {
+          train <- tab_agg[train.idx != i, ]
+          test <- tab_agg[train.idx == i, ]
+          set.seed(1409)
+          rg.irri <- ranger(treat ~ ., data = train, 
+                            num.trees = 500,
+                            mtry = function(x) n*x/n_mtry,
+                            importance = "impurity")
+          
+          pred.irri <- predict(rg.irri, data = test)
+          #data.frame(table(pred.irri$predictions, test$treat))
+          error <- rbind(error, data.frame(table(pred.irri$predictions, test$treat)))
+          rate <- c(rate, sum(test$treat != pred.irri$predictions)/nrow(test))
+          #print(table(test$irrigation, pred.irri$predictions))
+        }
+      } else {
+        train <- tab_agg[train.idx, ]
+        test <- tab_agg[-train.idx, ]
         set.seed(1409)
         rg.irri <- ranger(treat ~ ., data = train, 
                           num.trees = 500,
@@ -55,7 +76,6 @@ rf.mtry.taxo <- function(tab, tax.table, treat,
         data.frame(table(pred.irri$predictions, test$treat))
         error <- rbind(error, data.frame(table(pred.irri$predictions, test$treat)))
         rate <- c(rate, sum(test$treat != pred.irri$predictions)/nrow(test))
-        #print(table(test$irrigation, pred.irri$predictions))
       }
       TN <- c(TN, mean(error[error$Var1=="irr" & error$Var2=="irr","Freq"]))
       TP <- c(TP, mean(error[error$Var1=="non-irr" & error$Var2=="non-irr","Freq"]))
