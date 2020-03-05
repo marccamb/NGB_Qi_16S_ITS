19bbc28cb3c3379dbe1d5d34302e22f68d4efe24
marcamb <cambonmarine@gmail.com>
2020-03-05 17:37:31 +0100

    Added a function to grow n forests with blind validation

--- src_function_rf_mtry_taxo.R
+++ src_function_rf_mtry_taxo.R
@@ -23,6 +23,7 @@ rf.mtry.taxo <- function(tab, tax.table, treat,
   # treat <- z$irrigation
   # train.id <- "-M-"
   
+  # define train samples indices
   if (is.na(train.id)) {
     set.seed(seed)
     train.idx <- sample(rep(1:n_fold, 1/n_fold * ncol(tab)), replace = F)
@@ -66,14 +67,13 @@ rf.mtry.taxo <- function(tab, tax.table, treat,
       } else {
         train <- tab_agg[train.idx, ]
         test <- tab_agg[-train.idx, ]
-        set.seed(1409)
+        set.seed(seed)
         rg.irri <- ranger(treat ~ ., data = train, 
                           num.trees = 500,
                           mtry = function(x) n*x/n_mtry,
                           importance = "impurity")
         
         pred.irri <- predict(rg.irri, data = test)
-        data.frame(table(pred.irri$predictions, test$treat))
         error <- rbind(error, data.frame(table(pred.irri$predictions, test$treat)))
         rate <- c(rate, sum(test$treat != pred.irri$predictions)/nrow(test))
       }
@@ -108,7 +108,43 @@ rf.nfold <- function(tab, treat,
     set.seed(seed)
     rg.irri <- ranger(treat ~ ., data = train, 
                       num.trees = 500,
-                      mtry = 540,
+                      mtry = mtry,
+                      importance = "impurity")
+    
+    pred.irri <- predict(rg.irri, data = test)
+    error <- data.frame(table(pred.irri$predictions, test$treat))
+    err_rate <- sum(test$treat != pred.irri$predictions)/nrow(test)
+    TN <- error[error$Var1=="irr" & error$Var2=="irr","Freq"]
+    TP <- error[error$Var1=="non-irr" & error$Var2=="non-irr","Freq"]
+    FN <- error[error$Var1=="non-irr" & error$Var2=="irr","Freq"]
+    FP <- error[error$Var1=="irr" & error$Var2=="non-irr","Freq"]
+    sensitivity <- TP/(TP+FN)
+    precision <- TP/(TP+FP)
+    res <- rbind(res, cbind(TP, TN, FP, FN, err_rate, sensitivity, precision))
+    importance[[i]] <- rg.irri$variable.importance
+  }
+  res_tot <- list(res, importance)
+  names(res_tot) <- c("confusion", "importance")
+  return(res_tot)
+}
+
+rf.define.training <- function(tab, treat,
+                               train.id = NA, 
+                               mtry = NULL,
+                               n_forest=100) {
+  train.idx <- grep(train.id, colnames(tab))
+  tab <- data.frame("treat" = treat, t(tab))
+  train <- tab[train.idx, ]
+  test <- tab[-train.idx, ]
+  pred.irri <- error <- rate <- NULL
+  res <- data.frame()
+  importance <- list()
+  for (i in 1:n_forest) {
+    message("Growing forest number ", i, "...")
+    #set.seed(140)
+    rg.irri <- ranger(treat ~ ., data = train, 
+                      num.trees = 500,
+                      mtry = mtry,
                       importance = "impurity")
     
     pred.irri <- predict(rg.irri, data = test)
