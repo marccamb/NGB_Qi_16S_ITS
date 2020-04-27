f0dccfbc79c950363b32d3918bdb73e04c949f6f
Marine C. Cambon <cambonmarine@gmail.com>
2020-04-27 17:22:39 +0200

    Chaged the way NA in taxonomy are handled

--- R/agg.table.taxo.R
+++ R/agg.table.taxo.R
@@ -6,19 +6,21 @@
 #' \code{tax.lvl} should be the name of one colum of tax.table (case insensitive).
 #' The default is "genus".
 #' @return An abundance table with the sum of counts for each ASV/OTU corresponding to
-#' a given taxonomic level, found in each sample.
+#' a given taxonomic level, found in each sample. ASV/OTUs that are not assigned at this
+#' specific level are discarded.
 #' @import stats
 #'
 # 2019-11-28
 # Marine Cambon
 
 agg.table.taxo <- function(tab, tax.lvl="genus", tax.table) {
-  if (any(rownames(tab) != rownames(tax.table))) stop("The ASV/OTU table and the taxonomy table are ordered the same way")
+  if (any(rownames(tab) != rownames(tax.table))) stop("The ASV/OTU table and the taxonomy table should have the same
+                                                      row names. Check that they are ordered the same way")
   message(paste('Table aggregation to the', tax.lvl, "level."))
   #message('Please be sure that the ASV/OTU table and the taxonomy table are ordered the same way')
   if(nrow(tab) != nrow(tax.table)) stop("The ASV/OTU table and the taxonomy table do not have the same number of rows")
   tax <- tax.table[,grep(tax.lvl, colnames(tax.table), ignore.case = T)]
-  tax[is.na(tax)] <- "Unknown"
+  #tax[is.na(tax)] <- "Unknown"
   tab <- stats::aggregate(tab, by=list("taxo"=tax), FUN=sum)
   rownames(tab) <- tab[,1]
   tab <- tab[,-1]
