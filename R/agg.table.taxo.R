dce661275eaa1b3ec37129e01b75a67bf62d19b6
Marine C. Cambon <cambonmarine@gmail.com>
2020-04-30 18:29:24 +0200

    Added some documentation and allowed the specification of the taxo lvl to use in rf.opti.mtry

--- R/agg.table.taxo.R
+++ R/agg.table.taxo.R
@@ -14,8 +14,7 @@
 # Marine Cambon
 
 agg.table.taxo <- function(tab, tax.lvl="genus", tax.table) {
-  if (any(rownames(tab) != rownames(tax.table))) stop("The ASV/OTU table and the taxonomy table should have the same
-                                                      row names. Check that they are ordered the same way")
+  if (any(rownames(tab) != rownames(tax.table))) stop("ASVs/OTUs table and taxonomy table should have the same row names. \nCheck that they are ordered the same way")
   message(paste('Table aggregation to the', tax.lvl, "level."))
   #message('Please be sure that the ASV/OTU table and the taxonomy table are ordered the same way')
   if(nrow(tab) != nrow(tax.table)) stop("The ASV/OTU table and the taxonomy table do not have the same number of rows")
